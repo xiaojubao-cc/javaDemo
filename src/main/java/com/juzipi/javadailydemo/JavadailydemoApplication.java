c14f3343b756ed15a883ccfe0ac0a286bf9dc383
juzipi <1171264943@qq.com>
2023-01-10 00:54:15 +0800

    测试创建git远程仓库

---
+++ src/main/java/com/juzipi/javadailydemo/JavadailydemoApplication.java
@@ -0,0 +1,17 @@
+package com.juzipi.javadailydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 汪建超
+ */
+@SpringBootApplication
+public class JavadailydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavadailydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juzipi/javadailydemo/JavadailydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juzipi.javadailydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadailydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
